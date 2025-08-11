5b159d711a7e3142baafe7c42bc43b9b75be7a07
tachu <roatyungtatyu@gmail.com>
2025-08-11 22:27:57 +0900

    トークンの定義に関するコメントを追加し、トークンの構成要素についての説明を明確化

--- java/com/craftinginterpreters/lox/Token.java
+++ java/com/craftinginterpreters/lox/Token.java
@@ -1,5 +1,13 @@
 package com.craftinginterpreters.lox;
 
+/**
+ * A token is a lexical unit in the source code.
+ * It is a pair of a TokenType and a lexeme.
+ * The lexeme is the actual text of the token.
+ * The TokenType is the type of the token.
+ * The literal is the value of the token.
+ * The line is the line number of the token in the source code.
+ */
 public class Token {
     final TokenType type;
     final String lexeme;
