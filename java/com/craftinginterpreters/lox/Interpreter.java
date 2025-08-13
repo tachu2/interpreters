9e60ca3ae86b64e8a310e71a4ee449d7b992ba95
tachu <roatyungtatyu@gmail.com>
2025-08-14 00:51:56 +0900

    機能追加: Interpreterクラスにゼロ除算のチェックを追加し、ゼロでの除算時にRuntimeErrorをスローするように修正。

--- java/com/craftinginterpreters/lox/Interpreter.java
+++ java/com/craftinginterpreters/lox/Interpreter.java
@@ -82,6 +82,9 @@ public class Interpreter implements Expr.Visitor<Object> {
                 throw new RuntimeError(expr.operator, "Operands must be two numbers or two strings.");
             case SLASH:
                 checkNumberOperands(expr.operator, left, right);
+                if ((double)right == 0) {
+                    throw new RuntimeError(expr.operator, "Division by zero.");
+                }
                 return (double)left / (double)right;
             case STAR:
                 checkNumberOperands(expr.operator, left, right);
