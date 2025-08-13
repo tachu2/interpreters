5602f88c70fc022210264a2dfbd316c63a4082f9
tachu <roatyungtatyu@gmail.com>
2025-08-14 00:38:53 +0900

    機能追加: Interpreterクラスに式の評価と結果の出力機能を追加。Loxクラスにランタイムエラー処理を統合し、エラー発生時の終了コードを設定。

--- java/com/craftinginterpreters/lox/Interpreter.java
+++ java/com/craftinginterpreters/lox/Interpreter.java
@@ -2,6 +2,15 @@ package com.craftinginterpreters.lox;
 
 public class Interpreter implements Expr.Visitor<Object> {
 
+    void interpret(Expr expression) {
+        try {
+            Object value = evaluate(expression);
+            System.out.println(stringify(value));
+        } catch (RuntimeError error) {
+            Lox.runtimeError(error);
+        }
+    }
+
     @Override
     public Object visitLiteralExpr(Expr.Literal expr) {
         return expr.value;
@@ -114,6 +123,20 @@ public class Interpreter implements Expr.Visitor<Object> {
 
     private Object evaluate(Expr expr) {
         return expr.accept(this);
+    }
+
+    private String stringify(Object object) {
+        if (object == null) {
+            return "nil";
+        }
+        if (object instanceof Double) {
+            String text = object.toString();
+            if (text.endsWith(".0")) {
+                text = text.substring(0, text.length() - 2);
+            }
+            return text;
+        }
+        return object.toString();
     }
 
 
