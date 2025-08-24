4519b398e1f0f5a74251dd87245bd931cdc9e29a
tachu <roatyungtatyu@gmail.com>
2025-08-24 11:08:28 +0900

    構文解析機能の拡張: ast.bnfにブロック構文を追加し、StmtクラスにBlockステートメントを実装。MakefileにAST生成のためのターゲットを追加し、環境クラスに親環境を持つ機能を実装。GenerateAstツールを更新し、ASTの定義を強化。

--- Makefile
+++ Makefile
@@ -15,10 +15,15 @@ default: $(CLASSES)
 	@: # Don't show "Nothing to be done" output.
 
 # Compile a single .java file to .class.
-$(BUILD_DIR)/$(DIR)/%.class: $(DIR)/%.java
+$(BUILD_DIR)/$(DIR)/%.class: $(DIR)/%.java generate-ast
 	@ mkdir -p $(BUILD_DIR)/$(DIR)
 	@ javac -cp $(DIR) -d $(BUILD_DIR)/$(DIR) $(JAVA_OPTIONS) -implicit:none $<
 	@ printf "%8s %-60s %s\n" javac $< "$(JAVA_OPTIONS)"
+
+.PHONY: generate-ast
+generate-ast:
+	javac -cp $(DIR) -d $(BUILD_DIR)/$(DIR) $(JAVA_OPTIONS) $(DIR)/com/craftinginterpreters/tool/GenerateAst.java
+	java -cp $(BUILD_DIR)/$(DIR) com.craftinginterpreters.tool.GenerateAst $(DIR)/com/craftinginterpreters/lox
 
 .PHONY: run
 run: default
--- java/com/craftinginterpreters/lox/Environment.java
+++ java/com/craftinginterpreters/lox/Environment.java
@@ -4,7 +4,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Environment {
+    final Environment enclosing; // parent environment
     private final Map<String, Object> values = new HashMap<>();
+
+    Environment() {
+        enclosing = null;
+    }
+
+    Environment(Environment enclosing) {
+        this.enclosing = enclosing;
+    }
 
     void define(String name, Object value) {
         values.put(name, value);
@@ -18,11 +27,24 @@ public class Environment {
         throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
     }
 
+    /**
+     * Assigns a value to a variable in the current environment or in the enclosing environment.
+     * 
+     * @param name the token representing the variable
+     * @param value the value to assign to the variable
+     * @throws RuntimeError if the variable is not defined
+     */
     void assign(Token name, Object value) {
         if (values.containsKey(name.lexeme)) {
             values.put(name.lexeme, value);
             return;
         }
+
+        if (enclosing != null) {
+            enclosing.assign(name, value);
+            return;
+        }
+
         throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
     }
 
