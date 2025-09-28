89305f4ecb76093a1d3ecc7fc36a588198df7b22
tachu <roatyungtatyu@gmail.com>
2025-09-28 15:13:23 +0900

    スキャナー機能の拡張: 現在の文字を進めるadvance関数と、次の文字が期待される文字と一致するかを確認するmatch関数を追加。トークンをスキャンする際の処理を強化し、さまざまなトークンタイプに対応しました。

--- c/scanner.c
+++ c/scanner.c
@@ -3,7 +3,7 @@
 
 typedef struct {
     const char* start;
-    const char* current;
+    const char* current; // the next character to be scanned
     int line;
 } Scanner;
 
@@ -17,6 +17,33 @@ void initScanner(const char* source) {
 
 static bool isAtEnd() {
     return *scanner.current == '\0';
+}
+
+/**
+ * Advances the current character and returns the next character to be scanned.
+ * @return the next character to be scanned.
+ */
+static char advance() {
+    scanner.current++;
+    return scanner.current[-1];
+}
+
+/**
+ * Checks if the next character to be scanned matches the expected character.
+ * If it does, advances the current character and returns true.
+ * If it does not, returns false.
+ * @param expected the expected character.
+ * @return true if the next character to be scanned matches the expected character, false otherwise.
+ */
+static bool match(char expected) {
+    if (isAtEnd()) {
+        return false;
+    }
+    if (*scanner.current != expected) {
+        return false;
+    }
+    scanner.current++;
+    return true;
 }
 
 static Token makeToken(TokenType type) {
@@ -34,6 +61,56 @@ Token scanToken() {
 
     if (isAtEnd()) {
         return makeToken(TOKEN_EOF);
+    }
+
+    char c = advance();
+
+    switch (c) {
+        case '(': {
+            return makeToken(TOKEN_LEFT_PAREN);
+        }
+        case ')': {
+            return makeToken(TOKEN_RIGHT_PAREN);
+        }
+        case '{': {
+            return makeToken(TOKEN_LEFT_BRACE);
+        }
+        case '}': {
+            return makeToken(TOKEN_RIGHT_BRACE);
+        }
+        case ';': {
+            return makeToken(TOKEN_SEMICOLON);
+        }
+        case ',': {
+            return makeToken(TOKEN_COMMA);
+        }
+        case '.': {
+            return makeToken(TOKEN_DOT);
+        }
+        case '-': {
+            return makeToken(TOKEN_MINUS);
+        }
+        case '+': {
+            return makeToken(TOKEN_PLUS);
+        }
+        case '/': {
+            return makeToken(TOKEN_SLASH);
+        }
+        case '*': {
+            return makeToken(TOKEN_STAR);
+        }
+        case '!': {
+            return makeToken(match('=') ? TOKEN_BANG_EQUAL : TOKEN_BANG);
+        }
+        case '=': {
+            return makeToken(match('=') ? TOKEN_EQUAL_EQUAL : TOKEN_EQUAL);
+        }
+        case '<': {
+            return makeToken(match('=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
+        }
+        case '>': {
+            return makeToken(match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
+        }
     }
 
     return errorToken("Unexpected character.");
